f67476bfa34e63536f339c81df4350eb5100455b
kakuilan <kakuilan@163.com>
2019-08-08 21:38:10 +0800

    给组件设置多个属性

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,22 @@
 // 自定义组件
 import React from 'react';
+import Person from './Person';
 
 function Hello(props) {
-    return (
-        <h1>Hello World，{props.nickname}!</h1>
-    );
+  var someone = {
+    name: '张三',
+    sex: '先生',
+    age: '25',
+    addr: '海南',
+    tel: '13812345678'
+  };
+  return (
+    <div>
+      <h1>Hello World，{props.nickname}!</h1>
+      {/** 给子组件传递多属性*/}
+      <Person {...someone} />
+    </div>
+  );
 }
 
-export default Hello
+export default Hello;
